ed41b1f23c95f389ecdf5e311517aec50c9a5fb7
codemonkeydance <shannon.files@gmail.com>
2015-07-16 00:13:27 -0400

    moved data files into project, changed path in code

--- load_and_analyze.py
+++ load_and_analyze.py
@@ -7,13 +7,13 @@ logging.basicConfig(level=logging.INFO)
 
 
 CONSUMER_COMPLAINTS_CREATE_SQL = "sql/create_consumer_complaints.sql"
-CONSUMER_COMPLAINTS_CSV = "../data/Consumer_Complaints.csv"
+CONSUMER_COMPLAINTS_CSV = "data/Consumer_Complaints.csv"
 
 GEOTABLE_CREATE_SQL = "sql/create_g20135us.sql"
-GEOTABLE_CSV = "../data/g20135us.csv"
+GEOTABLE_CSV = "data/g20135us.csv"
 
 TMP_SEQ0015_CREATE_SQL = "sql/create_tmp_seq0015.sql"
-TMP_SEQ0015_CSV = "../data/e20135us0015000_header.csv"
+TMP_SEQ0015_CSV = "data/e20135us0015000_header.csv"
 
 #ESTIMATE_INCOME_DATA = "..data/e20135us0015000.txt"
 
